16099a0cf114430b903cd17e98367b1ead18002e
I am the Carl <jamescarl96@gmail.com>
2021-02-21 15:08:24 -0500

    Cleaned up some warnings.

--- freertos-rust/build.rs
+++ freertos-rust/build.rs
@@ -1,19 +1,6 @@
 use std::env;
 use std::path::PathBuf;
 
-fn print_env() {
-    let env_keys = ["TARGET", "OUT_DIR", "HOST"];
-    env::vars().for_each(|(key, val)| {
-        if key.starts_with("CARGO") {
-            println!("cargo:warning={}={}", key, val);
-        } else if env_keys.contains(&key.as_str()) {
-            println!("cargo:warning={}={}", key, val);
-        } else {
-            //println!("cargo:warning={}={}", key, val);
-        }
-    });
-}
-
 // See: https://doc.rust-lang.org/cargo/reference/build-scripts.html
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
